8c5f56df235ff1cca2bb217aa9e66fa6a08ac277
stnqq <life33890@gmail.com>
2026-01-31 17:20:46 +0500

    Practices + Bank Tasks

---
+++ Practices/NewTask.java
@@ -0,0 +1,25 @@
+package Practices;
+
+import java.sql.Array;
+import java.util.ArrayList;
+
+public class NewTask {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> speeds = new ArrayList<>(); // Объявите список
+        speeds.add(120);
+        speeds.add(75);
+        speeds.add(42);
+        speeds.add(60);
+        speeds.add(110);
+        speeds.add(20);
+
+        int sum = 0;
+        for (Integer speed : speeds) {
+            sum += speed;  // Сложите все значения в списке
+        }
+
+        int averageSpeed = sum / speeds.size(); // Вычислите среднюю скорость
+        System.out.println("Средняя скорость равна " + averageSpeed + " км/ч");
+    }
+}
