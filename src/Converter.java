2945aa2bb8703e12bd9e96e221d1ced4237fdf89
stnqq <life33890@gmail.com>
2026-01-31 22:19:44 +0500

    little changes with redundant variables and values

--- src/Converter.java
+++ src/Converter.java
@@ -3,13 +3,11 @@ package src;
 public class Converter {
     public int convertToKm(int steps) {
         // 1 step ~~ 0.75m
-        int km = (steps * 75) / 100000;
-        return km;
+        return (steps * 75) / 100000;
     }
 
     public int convertStepsToKilocalories(int steps) {
         // 1 step ~~ 0.05kcal for step
-        int kcal = (steps * 5) / 100;
-        return kcal;
+        return (steps * 5) / 100;
     }
 }
--- src/MonthData.java
+++ src/MonthData.java
@@ -43,8 +43,8 @@ public class MonthData {
         int currentSeries = 0;
         int finalSeries = 0;
 
-        for (int i = 0; i < days.length; i++) {
-            if (days[i] >= goalByStepsPerDay) {
+        for (int day : days) {
+            if (day >= goalByStepsPerDay) {
                 currentSeries++;
                 finalSeries = Math.max(finalSeries, currentSeries);
             } else {
