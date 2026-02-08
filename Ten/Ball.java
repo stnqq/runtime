03716cefbc4cff3f64701cf9ba97e017e9032449
stnqq <life33890@gmail.com>
2026-02-08 15:55:04 +0500

    https://github.com/practicetasks/java_tasks/tree/main/object_hashcode_equals/task_1
    Done task with lost and found

---
+++ Ten/Ball.java
@@ -0,0 +1,4 @@
+package Ten;
+
+public class Ball {
+}
---
+++ Ten/LAFTask.java
@@ -0,0 +1,30 @@
+package Ten;
+
+import java.util.ArrayList;
+
+public class LAFTask {
+    public static void main(String[] args) {
+        Ball ball = new Ball();
+        Accordion accordion = new Accordion();
+        Umbrella umbrella = new Umbrella();
+        Hat hat = new Hat();
+
+        // Сдаём в бюро находок найденные вещи
+        LostAndFoundOffice.put(ball);
+        LostAndFoundOffice.put(accordion);
+        LostAndFoundOffice.put(umbrella);
+
+        checkObject(accordion, "Aккордеон");
+        checkObject(ball, "Мяч");
+        checkObject(umbrella, "Зонт");
+        checkObject(hat, "Шляпа");
+    }
+
+    private static void checkObject(Object object, String description) {
+        if(LostAndFoundOffice.check(object)) {
+            System.out.println('\'' + description + "' нашёлся!");
+        } else {
+            System.out.println('\'' + description + "' в бюро находок никто не приносил :(");
+        }
+    }
+}
